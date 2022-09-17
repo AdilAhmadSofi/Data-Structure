c85b6cea2a4d32ad9e610c74cd1d8a97d681758e
ayy_dee_eye_ell <Adilahmadsofi@gmail.com>
2022-09-17 15:11:05 +0530

    updated

--- Sorting/sortingheader.c
+++ Sorting/sortingheader.c
@@ -21,11 +21,8 @@ void swap(int arr[], int i, int j) {
 void insertion_sort(int arr[], size_t size) {
   for(int i=1; i<size;i++) {
         for(int j=i;j>0;j--) {
-            if(arr[j]<arr[j-1]) {
-                int temp=arr[j];
-                arr[j]= arr[j-1];
-                arr[j-1]=temp;
-            }            
+            if(arr[j]<arr[j-1])
+              swap(arr, j , j-1);
         }
     }
 }
@@ -42,10 +39,8 @@ void selection_sort(int arr[], size_t size) {
 }
 
 void bubbleSortArray(int arr[], size_t s) {
-	for(int i = 0; i < s-1; i++)
-	{
-		for(int j = 0;j < s-1-i; j++)
-		{
+	for(int i = 0; i < s-1; i++)	{
+		for(int j = 0;j < s-1-i; j++)		{
 			if(arr[j] > arr[j+1])
          swap(arr, j, j+1);
 		}
